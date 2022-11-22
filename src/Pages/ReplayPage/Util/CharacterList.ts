5435766bb7cd0422a0c7bc676f6411cd9522622d
Private User <43048964+githubuser720@users.noreply.github.com>
2022-11-21 21:30:53 -0500

    MOD: id not needed

--- src/Pages/ReplayPage/Util/CharacterList.ts
+++ src/Pages/ReplayPage/Util/CharacterList.ts
@@ -2,151 +2,126 @@ export const CharacterList = [
     {
         value: 'SO',
         label: 'Sol',
-        id: 1,
         imgUrl: 'SO.png'
     }, 
     {
         value: 'KY',
         label: 'Ky',
-        id: 2,
         imgUrl: 'KY.png'
     }, 
     {
         value: 'MA',
         label: 'May',
-        id: 3,
         imgUrl: 'MA.png'
     }, 
     {
         value: 'MI',
         label: 'Millia',
-        id: 4,
         imgUrl: 'MI.png'
     }, 
     {
         value: 'AX',
         label: 'Axl',
-        id: 5,
         imgUrl: 'AX.png'
     }, 
     {
         value: 'PO',
         label: 'Potemkin',
-        id: 6,
         imgUrl: 'PO.png'
     }, 
     {
         value: 'CH',
         label: 'Chipp',
-        id: 7,
         imgUrl: 'CH.png'
     }, 
     {
         value: 'ED',
         label: 'Eddie',
-        id: 8,
         imgUrl: 'ED.png'
     }, 
     {
         value: 'BA',
         label: 'Baiken',
-        id: 9,
         imgUrl: 'BA.png'
     }, 
     {
         value: 'FA',
         label: 'Faust',
-        id: 10,
         imgUrl: 'FA.png'
     }, 
     {
         value: 'TE',
         label: 'Testament',
-        id: 11,
         imgUrl: 'TE.png'
     }, 
     {
         value: 'JA',
         label: 'Jam',
-        id: 12,
         imgUrl: 'JA.png'
     }, 
     {
         value: 'AN',
         label: 'Anji',
-        id: 13,
         imgUrl: 'AN.png'
     }, 
     {
         value: 'JO',
         label: 'Johnny',
-        id: 14,
         imgUrl: 'JO.png'
     }, 
     {
         value: 'VE',
         label: 'Venom',
-        id: 15,
         imgUrl: 'VE.png'
     }, 
     {
         value: 'DI',
         label: 'Dizzy',
-        id: 16,
         imgUrl: 'DI.png'
     }, 
     {
         value: 'SL',
         label: 'Slayer',
-        id: 17,
         imgUrl: 'SL.png'
     }, 
     {
         value: 'IN',
         label: 'I-No',
-        id: 18,
         imgUrl: 'IN.png'
     }, 
     {
         value: 'ZA',
         label: 'Zappa',
-        id: 19,
         imgUrl: 'ZA.png'
     }, 
     {
         value: 'BR',
         label: 'Bridget',
-        id: 20,
         imgUrl: 'BR.png'
     }, 
     {
         value: 'RO',
         label: 'Robo-Ky',
-        id: 21,
         imgUrl: 'RO.png'
     }, 
     {
         value: 'AB',
         label: 'A.B.A',
-        id: 22,
         imgUrl: 'AB.png'
     }, 
     {
         value: 'OS',
         label: 'Order-Sol',
-        id: 23,
         imgUrl: 'OS.png'
     }, 
     {
         value: 'KL',
         label: 'Kliff',
-        id: 24,
         imgUrl: 'KL.png'
     }, 
     {
         value: 'JU',
         label: 'Justice',
-        id: 25,
         imgUrl: 'JU.png'
     }
 ];
